caeadb8acd0946b2e6009e22ec389a6cc7673db6
Shraddha Dharmik <49453241+shraddhadharmik@users.noreply.github.com>
2019-05-09 16:15:57 +0530

    Add files via upload

--- DML Commands.sql
+++ DML Commands.sql
@@ -101,3 +101,142 @@ insert into test(salary) values(8888.88);
 insert into test(salary) values(879888.88);  #value out of range
 select* from test;
 
+
+#------------------------------------------SELECT---------------------------------------
+use df1903cm;
+
+desc user;
+
+insert into user values(100,'Archana', 'Well Educated', 56.97, False, '1998/08/10');
+
+insert into user values(101,'Sagar', 'Not well', 10.97, True, '1970/08/17');
+insert into user values(102,'Suyash', 'Golu', 100.97, True, '1960/08/17');
+
+select * from user;
+
+create table persons(
+id int not null auto_increment,
+lastname varchar(255) not null,
+firstname varchar(255),
+address varchar(255),
+primary key(id))
+auto_increment = 100;
+
+insert into persons(firstname,lastname) values
+('sdfs','sdfsd'),
+('eger','rthswe');
+
+
+#-----------------UPDATE---------------------------
+
+select * from user;
+
+set sql_safe_updates = 0;
+update students set marks = 101 where name='Sameer';
+
+
+#------------------------DELETE----------------
+delete from persons;
+
+#delete for a person
+delete from user where id=102;
+
+#delete for multiple
+delete from user where id=100 or 101;
+select * from user;
+
+use mydb;
+
+desc customer;
+desc contact;
+
+insert into customer values
+('25','sajkda','sdfewwe');
+
+insert into contact values
+('25','24','sdfewwe');
+
+select * from customer;
+select * from contact;
+update contact set info='dsfsdwe' where customer_id=24;
+
+delete from customer where id=28;
+
+update customer set id=28 where id=24;
+
+use df1903cm;
+
+create table students1(
+id int auto_increment primary key, name varchar(20), class varchar(20), 
+social int, science int, math int);
+
+INSERT INTO students1 (id, name, class, social, science, math)
+ VALUES (2, 'Max Ruin', 'Three', 86, 57, 86);
+ 
+ INSERT INTO students1 (id, name, class, social, science, math)VALUES
+(2, 'Max Ruin', 'Three', 86, 57, 86),
+(3, 'Arnold', 'Three', 56, 41, 76),
+(4, 'Krish Star', 'Four', 62, 52, 72),
+(5, 'John Mike', 'Four', 62, 82, 92),
+(6, 'Alex John', 'Four', 58, 93, 83),
+(7, 'My John Rob', 'Fifth', 79, 64, 74),
+(8, 'Asruid', 'Five', 89, 84, 94),
+(9, 'Tes Qry', 'Six', 77, 61, 71),
+(10, 'Big John', 'Four', 56, 44, 56) 
+
+ON DUPLICATE KEY UPDATE social=values(social),
+science=values(science),math=values(math);
+ 
+ select * from students1;
+ 
+ INSERT INTO students1 (id, name, class, social, science, math) VALUES
+(2, 'Max Ruin', 'Three', 86, 88, 88),
+(3, 'Arnold', 'Three', 56, 41, 76),
+(4, 'Krish Star', 'Four', 62, 52, 72),
+(5, 'John Mike', 'Four', 62, 82, 92),
+(6, 'Alex John', 'Four', 58, 93, 83),
+(7, 'My John Rob', 'Fifth', 79, 64, 74),
+(8, 'Asruid', 'Five', 89, 84, 94),
+(9, 'Tes Qry', 'Six', 77, 61, 71),
+(10, 'Big John', 'Four', 56, 44, 56),
+(11,'New Name','Five',75,78,52) 
+
+ON DUPLICATE KEY UPDATE social=values(social),science=values(science),math=values(math);
+ 
+INSERT INTO students1 (id, name, class, social, science, math)
+VALUES (2, 'Max Ruin', 'Three', 86, 57, 86) 
+on duplicate key update social=88,science=90,math=95;
+ 
+ UPDATE students1 SET math=50, social=60, science=55 WHERE  id=4;
+ 
+ UPDATE students1 SET math=0;
+ 
+ select * from students1;
+ 
+UPDATE students1 SET math=math+5, social=social+5, science=science+5 
+WHERE  id=3;
+
+create table marks(
+id int,
+name varchar(10),
+marks1 int,
+marks2 int);
+
+load data infile "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\marks.txt" into table marks fields terminated by ',';
+
+select * from marks;
+
+create table test_table(
+a int,
+b int
+);
+
+insert into test_table values(1,2),(2,3),(3,4);
+
+select a,b,a+b into outfile "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\result.txt" 
+fields terminated by ',' optionally enclosed by '"' 
+lines terminated by '\n' from test_table;
+
+
+
+
